cf610c29087af378cabeef3b0e86676abb959751
nghihai <nghi84072@gmail.com>
2023-10-22 20:40:26 -0700

    Complete Practice with Functions

--- js/01-functions-practice.js
+++ js/01-functions-practice.js
@@ -1,9 +1,33 @@
 //STEP 1
+function halfNumber(number) {
+    console.log(`Half of ${number} is ${number/2}`)
+}
 
 //STEP 2
+function squareNumber(number) {
+    console.log(`The result of squaring the number ${number} is ${number * number}`)
+}
 
 //STEP 3
+function percentOf(firstNum, secondNum)
+{
+    let percent = (firstNum * 100)/secondNum
+    console.log(`${firstNum} is ${percent}% of ${secondNum}`)
+}
 
 //STEP 4
+function findModulus(firstNum,secondNum)
+{
+    let modulus
+    if (firstNum > secondNum)
+        modulus = firstNum % secondNum
+    else 
+        modulus = secondNum % firstNum
+    console.log(`${modulus} is the modulus of ${firstNum} and ${secondNum}`)
+}
 
-//STEP 5
+//STEP 5: Call all the functions
+halfNumber(5)
+squareNumber(3)
+percentOf(2, 4)
+findModulus(4,10)
