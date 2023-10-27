aa5a2321544f1545a46f48416f48caa37f7ac441
nghihai <nghi84072@gmail.com>
2023-10-26 20:30:37 -0700

    Complete rock, paper, scissors

--- js/02-rock-paper-scissors.js
+++ js/02-rock-paper-scissors.js
@@ -21,9 +21,9 @@ const user = function ()
     }
 
     //Display the user choice
-    if (userChoice === 1) {alert("You choose Rock")}
-    else if (userChoice === 2) {alert("You choose Paper")}
-    else {alert("You choose Scissors")}
+    if (userChoice === 1) {alert("You chose Rock")}
+    else if (userChoice === 2) {alert("You chose Paper")}
+    else {alert("You chose Scissors")}
 
     return userChoice   
 }
@@ -31,25 +31,57 @@ const user = function ()
 /*
 This funtion will generate computer's choice as a number
 display computer's choice as a string and return the choice number
-
+*/
 const computer = function () 
 {
+    //Generate random computer's choice 
+    let computerChoice = Math.floor(Math.random() * 2 + 1)
 
+    //Display the user choice
+    if (computerChoice === 1) {alert("Computer chose Rock")}
+    else if (computerChoice === 2) {alert("Computer chose Paper")}
+    else {alert("Computer chose Scissors")}
+
+    return computerChoice
 }
 
 /*
 This function will compare the two result of user and computer 
 then display the result 
+*/
+function gameResult(userChoice, computerChoice) {
+    //handle the tie situation
+    //alert(userChoice)
+    //alert(computerChoice)
+    while (userChoice == computerChoice) {
+        alert("Tie")
+        userChoice = user()
+        computerChoice = computer()
+    }
 
-function gameResult(user, computer) {
+    if ((userChoice == 1 && computerChoice == 3) || (userChoice == 3 && computerChoice == 2) || (userChoice == 2 && computerChoice == 1))
+    {   
+        alert("You Win!")
+    }
+    else 
+    {
+        alert("Computer Win!")
+    }
 
 }
-*/
-//main function to run the game
-function main(){
-    displayGame()
-    user()
 
+//main function to run the game
+function main()
+{
+    do
+    {
+        displayGame()
+        gameResult(user(),computer())
+        playGame = prompt("Do you want to play again? y/n")
+
+    } while(playGame === 'y')
+
+    
 }
 
 main()
