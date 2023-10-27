ddf8a7619e387f9da0bea7f771ac5b690b25b076
nghihai <nghi84072@gmail.com>
2023-10-26 21:54:49 -0700

    Complete the assignment

--- js/03-basic-calculator.js
+++ js/03-basic-calculator.js
@@ -1,10 +1,39 @@
 // ADD A FUNCTION CALLED CALCULATE
+function calculate(firstNum, secondNum, operation) 
+{
+    let result
+    switch (operation){
+        case "+":
+            result = firstNum + secondNum
+            break
+        case "-":
+            result = firstNum - secondNum
+            break
+        case "*":
+            result = firstNum * secondNum
+            break
+        default:
+            result = firstNum / secondNum
+    }
+    return result 
+}
 
+function display()
+{
+    // COLLECT FIRST NUMBER FROM USER
+    firstNum = parseInt(prompt("Please enter the first number: "))
 
-// COLLECT FIRST NUMBER FROM USER
+    // COLLECT SECOND NUMBER FROM USER
+    secondNum = parseInt(prompt("Please enter the second number: "))
 
-// COLLECT SECOND NUMBER FROM USER
+    // COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
+    do 
+    {
+        operation = prompt("Please enter the operation that you want to perform (+, -, *, /): ")
+    } while (operation != "+" && operation != "-" && operation != "*" && operation != "/")
 
-// COLLECT OPERATION TO PERFORM (+,-,*,/) FROM USER
+    // CALL THE FUNCTION AND RETURN THE RESULT WITHIN AN ALERT
+    alert(`The result is ${calculate(firstNum,secondNum,operation)}`)
+}
 
-// CALL THE FUNCTION AND RETURN THE RESULT WITHIN AN ALERT
+display()
